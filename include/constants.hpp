e3cfd8f895ff280797169e10aa1c123466a0c5b1
Arin <aringrigoras@gmail.com>
2023-02-04 21:37:51 +0200

    the rest of the operations

---
+++ include/constants.hpp
@@ -0,0 +1,14 @@
+#ifndef CML_CONSTANTS_HPP
+#define CML_CONSTANTS_HPP
+
+
+namespace cml{
+
+    static constexpr float pi = 3.14159265358979323846f;
+    static constexpr float degreesToRadians = pi / 180.0f;
+    static constexpr float radiansToDegrees = 180.0f / pi;
+
+};//namespace cml
+
+
+#endif //CML_CONSTANTS_HPP
--- include/vector2d.hpp
+++ include/vector2d.hpp
@@ -1,5 +1,5 @@
-#ifndef VECTOR3D_HPP
-#define VECTOR3D_HPP
+#ifndef CML_VECTOR3D_HPP
+#define CML_VECTOR3D_HPP
 
 #include <cmath>
 
@@ -24,21 +24,27 @@ namespace cml{
                 return Vector2(x-v.x, y-v.y);
             }
 
-            //scalar-multiplication for Vector2d
-            Vector2 scalar(float sc, Vector2 v);
-            //cross-operator for Vector2d
-            Vector2 cross(Vector2 a, Vector2 b);
-            //dot-operator for Vector2d
-            float dot(Vector2 a, Vector2 b);
-            //magnitude for Vector2D
-            float magnitude(Vector2 v);
-            
+    };//class Vector2;
 
-    };
+    // scalar-multiplication for Vector2d
+    Vector2 scalar(float sc, Vector2 v);
+    // cross-operator for Vector2d
+    Vector2 cross(Vector2 a, Vector2 b);
+    // dot-operator for Vector2d
+    float dot(Vector2 a, Vector2 b);
+    // magnitude for Vector2D
+    float magnitude(Vector2 v);
+    // the angle between a vector and the positive x-axis
+    float angle(Vector2 v);
+    // unit of vector2d
+    Vector2 unit(Vector2 v);
+    // computing the projection of `b` onto `a`
+    Vector2 projection(Vector2 a, Vector2 b);
+    //computing the scalar projection of `b` onto `a`
+    float scalar_projection(Vector2 a, Vector2 b);
 
-    
 
-};
+};//namespace cml
 
 
 #endif //VECTOR3D_HPP
