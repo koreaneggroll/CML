fda4f31508790e7333668c50d190b8ca059d422d
Arin <aringrigoras@gmail.com>
2023-02-14 23:26:51 +0200

    pow function

--- include/cml.hpp
+++ include/cml.hpp
@@ -10,7 +10,11 @@ namespace cml{
    //functions
     //calculates the hypot. of a right triangle
     double hypot(double catheti1, double catheti2);
+    //fast inverse square root
     float isqrt(float number);
+    //`pow` function
+    float pow(float a, float b);
+    
 
 };//namespace cml
 
