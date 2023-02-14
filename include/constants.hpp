9b7aeccf7819f9e9d1748824d67e991759dbe346
Arin <aringrigoras@gmail.com>
2023-02-15 00:08:33 +0200

    Update constants.hpp

--- include/constants.hpp
+++ include/constants.hpp
@@ -24,6 +24,9 @@ namespace cml{
     //multiplication value to convert radians to degrees
     static constexpr long double radiansToDegrees = 180.0f / pi;
 
+    //eulers number
+    static constexpr long double e = 2.71828182845904523536;
+
     //2D vector of zeros
     static const cml::Vector2 zeros2f(0.0f, 0.0f);
     //2D vector of ones
