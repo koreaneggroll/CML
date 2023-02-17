a1053fdf5da570c70bb6e68ee8c7283f47c10de0
Arin <aringrigoras@gmail.com>
2023-02-18 00:00:28 +0200

    fixed bug

--- include/math.hpp
+++ include/math.hpp
@@ -12,22 +12,14 @@
 #include "./sqrt.hpp"
 #include "./radians_degrees.hpp"
 
-#include <type_traits>
+
 
 namespace cml{
-    template<
-    typename T, //real type
-    typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
-    T max(T a, T b){
-        return (a > b ? a : b);
-    }
+    int max(int a, int b);
+    float max(float a, float b);
 
-    template<
-    typename T, //real type
-    typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
-    T min(T a, T b){
-        return (a > b ? b : a);
-    }
+    int min(int a, int b);
+    float min(float a, float b);
 };
 
 
--- src/math.cpp
+++ src/math.cpp
@@ -1,29 +1,18 @@
 #include "../include/math.hpp"
 
-
- #define MAX(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-
-#define MIN(a, b) \
-    ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _b : _a; })
-
 int cml::max(int a, int b){
-    return MAX(a,b);
+    return (a > b ? a : b);
 }
 
 float cml::max(float a, float b){
-    return MAX(a,b);
+    return (a > b ? a : b);
 }
 
 
 int cml::min(int a, int b){
-    return MAX(a,b);
+    return (a > b ? b : a);
 }
 
 float cml::min(float a, float b){
-    return MAX(a,b);
+    return (a > b ? b : a);
 }
