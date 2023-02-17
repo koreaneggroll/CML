e5ca27b9ea0268c929e74153455cd040aba96e65
Arin <aringrigoras@gmail.com>
2023-02-17 21:51:41 +0200

    math.hpp

--- include/cml.hpp
+++ include/cml.hpp
@@ -1,14 +1,7 @@
 #ifndef CML_CML_HPP
 #define CML_CML_HPP
 
-#include "./sqrt.hpp"
-#include "./pow.hpp"
-#include "./isqrt.hpp"
-#include "./hypot.hpp"
-#include "./abs.hpp"
-#include "./bin.hpp"
-#include "./fma.hpp"
-#include "./radians_degrees.hpp"
+#include "./math.hpp"
 #include "./vector2d.hpp"
 #include "./vector3d.hpp"
 #include "./constants.hpp"
---
+++ include/math.hpp
@@ -0,0 +1,16 @@
+#ifndef CML_MATH_HPP
+#define CML_MATH_HPP
+
+
+#include "./abs.hpp"
+#include "./bin.hpp"
+#include "./fma.hpp"
+#include "./hypot.hpp"
+#include "./isqrt.hpp"
+#include "./pow.hpp"
+#include "./trig.hpp"
+#include "./sqrt.hpp"
+#include "./radians_degrees.hpp"
+
+
+#endif //CML_MATH_HPP
